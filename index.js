2bc70f74beeea8338d3e0a48b90573aa337b8ffd
Mr.zhang <793338023@qq.com>
2021-05-17 22:25:50 +0800

    增加路由中间件

--- index.js
+++ index.js
@@ -1,10 +1,49 @@
 const MKoa = require("./lib/index");
+const static = require("./lib/static");
+const Router = require("./lib/router");
 
 const app = new MKoa();
+const router = new Router();
 
-app.use((req) => {
-  console.log(req.method);
-  return "abcs";
+const delay = () => new Promise((res) => setTimeout(() => res(), 2000));
+
+app.use(static());
+
+// app.use(async (ctx, next) => {
+//   console.log("开启001");
+//   ctx.body = "001";
+//   await next();
+// });
+
+// app.use(async (ctx, next) => {
+//   ctx.body += " 002";
+//   await delay();
+//   await next();
+// });
+
+// app.use(async (ctx, next) => {
+//   ctx.body = " 003";
+//   await next();
+// });
+
+// app.use(async (ctx, next) => {
+//   console.log("结束004");
+//   ctx.body = " 004";
+//   await next();
+// });
+
+router.get("/", (ctx, next) => {
+  ctx.body = "home";
 });
+
+router.get("/index", (ctx, next) => {
+  ctx.body = "index";
+});
+
+router.post("/index", (ctx, next) => {
+  ctx.body = { a: "xxx" };
+});
+
+app.use(router.routers());
 
 app.listen(3000);
